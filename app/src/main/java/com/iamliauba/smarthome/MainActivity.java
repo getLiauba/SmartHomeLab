fb634c0f8f76fe59acd023ffc9a05f6f77199172
getLiauba <36175885+getLiauba@users.noreply.github.com>
2019-10-15 23:34:43 -0400

    Finished home screen

--- app/src/main/java/com/iamliauba/smarthome/MainActivity.java
+++ app/src/main/java/com/iamliauba/smarthome/MainActivity.java
@@ -1,7 +1,11 @@
 package com.iamliauba.smarthome;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frament_container, new HomeScreen()).commit();
+
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.nav_home:
+                            selectedFragment = new HomeScreen();
+
+                            break;
+
+                        case R.id.nav_settings:
+                            selectedFragment = new SettingsScreen();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frament_container,selectedFragment).commit();
+                    return true;
+                }
+            };
+
 }
